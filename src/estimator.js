b18a2be69727636b34c6a8042cad868060985b90
Uwem Uke <slyart147@gmail.com>
2020-04-09 18:17:09 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,88 @@
-const covid19ImpactEstimator = (data) => data;
+const data = {
+	region: {
+		name: "Africa",
+		avgAge: 19.7,
+		avgDailyIncomeInUSD: 5,
+		avgDailyIncomePopulation: 0.71
+	},
+	periodType: "days",
+	timeToElapse: 38,
+	reportedCases: 2747,
+	population: 66622705,
+	totalHospitalBeds: 1380614
+}
 
-export default covid19ImpactEstimator;
+// Challenge 1
+
+const currentlyInfected_impact = (data) => {
+	return data.reportedCases * 10;
+}
+
+const currentlyInfected_severeImpact = (data) => {
+	return data.reportedCases * 50;
+}
+
+const periodType = data => {
+	if (data.periodType === 'days') {
+		return data.timeToElapse/3;
+	}
+	if (data.periodType === 'weeks') {
+		var days = data.timeToElapse * 7;
+		return days/3;
+	}
+	if (data.periodType === 'months') {
+		var months = data.timeToElapse * 30;
+		return months/3;
+	}
+}
+
+const infectionsByRequestedTime_impact = (data) => {
+	var currentlyInfected = data.reportedCases * 10;
+	var factor = periodType(data);
+	console.log('factor ', factor)
+	var toFixed = Math.trunc(factor);
+	console.log('toFixed ', toFixed)
+	return currentlyInfected * Math.pow(2, toFixed);
+}
+
+const infectionsByRequestedTime_severeImpact = (data) => {
+	var currentlyInfected = data.reportedCases * 50;
+	var factor = periodType(data);
+	console.log('factor ', factor)
+	var toFixed = Math.trunc(factor);
+	console.log('toFixed ', toFixed)
+	return currentlyInfected * Math.pow(2, toFixed);
+}
+
+// Challenge 2
+
+const severeCasesByRequestedTime_impact = data => {
+	return 15/100 * infectionsByRequestedTime_impact(data);
+}
+
+const severeCasesByRequestedTime_severeImpact = data => {
+	return 15/100 * infectionsByRequestedTime_severeImpact(data);
+}
+
+const covid19ImpactEstimator = (data) => {
+
+	return console.log({
+		data: data, // the input data you got
+		estimates: {
+			impact: {
+				currentlyInfected: currentlyInfected_impact(data),
+				infectionsByRequestedTime: infectionsByRequestedTime_impact(data),
+				severeCasesByRequestedTime: severeCasesByRequestedTime_impact(data)
+			}, // your best case estimation
+			severeImpact: {
+				currentlyInfected: currentlyInfected_severeImpact(data),
+				infectionsByRequestedTime: infectionsByRequestedTime_severeImpact(data),
+				severeCasesByRequestedTime: severeCasesByRequestedTime_severeImpact(data)
+			} // your severe case estimation
+		}	
+	})
+};
+
+covid19ImpactEstimator(data);
+
+//export default covid19ImpactEstimator;
